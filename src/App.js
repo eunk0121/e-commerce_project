b8280db95803272d8b221842dae356708542b3d9
Eunkyoung Lee/assistant manager/LGSI Model Team(eunkyoung.lee@lge.com) <eunkyoung.lee@lge.com>
2021-02-05 18:08:49 +0530

    day 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message';
+import Botton from './components/Botton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Botton/>
       </header>
     </div>
   );
