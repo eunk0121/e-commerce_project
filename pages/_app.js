2f76a655d1e76bed216462203df76e775a4e7138
Eunkyoung Lee/assistant manager/LGSI Model Team(eunkyoung.lee@lge.com) <eunkyoung.lee@lge.com>
2021-03-02 17:24:38 +0530

    Day 16 completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,30 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+  background: linear-gradient(to right, #c9d6ff, #e2e2e2);
+  font-family: 'Lato', sans-serif;
+  color: #444;
+`;
+
+const Page = styled.div`
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+`;
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Component {...pageProps} />
-    </>
+    <Container>
+      <Normalize />
+      <Navbar />
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </Container>
   );
 };
 
